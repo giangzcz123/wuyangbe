e0532458d3f616628054952c9288cb89b795aef1
Xuan Bach <bachvijt@gmail.com>
2026-06-17 12:32:19 +0700

    Hoan thien phan order cua kitchen

--- api/kitchen_orders.php
+++ api/kitchen_orders.php
@@ -2,7 +2,7 @@
 require_once('../config/db.php');
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
-
+//
 try {
     $stmt = $pdo->prepare(
         "SELECT oi.OrderItemID, oi.ProductID, p.ProductName, 
